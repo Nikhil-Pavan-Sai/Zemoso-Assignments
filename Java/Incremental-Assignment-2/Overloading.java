0c3e3ff75211c12d74fd3fb97c5d75e4c1bd64e2
Nikhil-Pavan-Sai <9701853281mnps@gmail.com>
2019-06-20 14:59:20 +0530

    Overloading Problem

---
+++ Java/Incremental-Assignment-2/Overloading.java
@@ -0,0 +1,24 @@
+public class Overloading
+{
+    Overloading()
+    {
+        this("This is not Default constructor");
+        System.out.println("The Default constructor");
+    }
+
+    Overloading(String x)
+    {
+        this(5, 15);
+        System.out.println(x);
+    }
+
+    Overloading(int x, int y)
+    {
+        System.out.println("This is number constructor " + x * y); 
+    }
+
+    public static void main(String args[])
+    {
+        new Overloading();
+    }
+}
