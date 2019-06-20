0d31d722175d3193c45b41bace65cb65e7fafdf2
Nikhil-Pavan-Sai <9701853281mnps@gmail.com>
2019-06-20 11:40:14 +0530

    assignments

---
+++ Java/nikhil/assignment/data/JavaIncrAss1.java
@@ -0,0 +1,22 @@
+package nikhil.assignment.data;
+
+import java.util.*;
+
+public class JavaIncrAss1{
+	
+	int num;
+	char name;
+
+	public void getValues(){
+	
+		System.out.println(num + " " + name);
+	}
+
+
+	/*public void getLocalValues(){
+	
+		int n1;
+		char n2;
+		System.out.println(n1 + " " + n2);
+	}*/
+}
