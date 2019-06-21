1f5c971a13d250ced7a6e59ddf669bb4d45ebe23
Nikhil-Pavan-Sai <9701853281mnps@gmail.com>
2019-06-21 16:06:10 +0530

    Inheritance Third

--- Java/Incremental-Assignment-2/Vampire.java
+++ Java/Incremental-Assignment-2/Vampire.java
@@ -1,3 +1,15 @@
+/* Question
+
+A vampire number v is a number with an even number of digits n, that can be
+factored into two numbers x and y each with n/2 digits and not both with
+trailing zeroes, where v contains precisely all the digits from x and from y,
+in any order. Write a java program to print first 100 vampire numbers.
+
+Example for vampire number : 2160 is a vampire numbers
+12 * 60 = 2160
+*/
+
+
 import java.lang.Math;
 
 class Vampire{
