c8ac28f9ec235b862724f56d61d4a6cf9abe7ed3
Nikhil-Pavan-Sai <9701853281mnps@gmail.com>
2019-09-13 15:40:31 +0530

    Refactored dirs

--- SpringPracticeAssignments/Cities/src/main/java/com/nikhil/Cities/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nikhil.Cities.repository;
-
-import com.nikhil.Cities.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends JpaRepository<City,Long> {
-}
